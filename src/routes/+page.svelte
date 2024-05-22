18778e1a7e2f00625585e6166167754f8bc4673a
Mark Foresta <Mark.Foresta691@gmail.com>
2024-05-21 20:43:18 -0400

    Put in a very basic grid layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,13 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 <script>
   import Binkboard_Carousel from './Binkboard_Carousel.svelte';
-  console.log("MARK IS HERE")
+  import TournamentBinkboard from './Tournament_binkboard.svelte';
 </script>
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">NAIGO IS HERE</h1>
-		<p>Start by exploring:</p>
-		<ul>
-		<Binkboard_Carousel></Binkboard_Carousel>
-		</ul>
-	</div>
+<div class="ring-2 ring-white h-screen w-screen grid grid-cols-3 grid-rows-2 justify-center text-center items-center">
+	<div class=" ring-2 ring-white "><TournamentBinkboard></TournamentBinkboard></div>
+	<div class=" ring-2 ring-white ">2</div>
+	<div class=" ring-2 ring-white ">3</div>
+	<div class=" ring-2 ring-white ">4</div>
+	<div class=" ring-2 ring-white ">5</div>
+	<div class=" ring-2 ring-white ">6</div>
 </div>
